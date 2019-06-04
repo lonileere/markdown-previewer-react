7bf630db59a0218b58e6d0a7166f762e765bafef
Leke <l.onileere@gmail.com>
2019-06-04 20:38:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container">
+  <div className="row">
+    <div className="col-md-12">
+      <div className="jumbotron">
+        <h1 className="display-4">Markdown Previewer</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+          to featured content or information.</p>
+      </div>
     </div>
+  </div>
+  <div className="row">
+    <div className="col-md-6 offset-md-3">
+      <textarea type="text" className="form-control" rows="10" ></textarea>
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-md-6 col-offset-md-3">
+
+    </div>
+  </div>
+</div>
   );
 }
 
